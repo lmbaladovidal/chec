745b976717b85990b4128cd8f268fb7faabd9538
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-05-25 13:34:14 -0300

    inicio CRUD para Nuestras Recertas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,9 +3,6 @@ const path = require('path');
 const cervezasFilePath = path.join(__dirname, '../DataBase/BDCervezas.json');
 const cervezas = JSON.parse(fs.readFileSync(cervezasFilePath,"utf-8"));
 
-const recetasFilePath = path.join(__dirname, '../DataBase/bbddRecetas.json');
-const recetas = JSON.parse(fs.readFileSync(recetasFilePath,"utf-8"));
-
 const controllerMain = {
     index: (req,res) => {
         res.render('index',{cervezas:cervezas});
@@ -15,13 +12,7 @@ const controllerMain = {
     },
     enDesarrollo: (req,res) => {
         res.render('enDesarrollo');
-    },
-    recetas: (req,res) => {
-        // logica CRUD - mover a nuevo controller de nuevas recetas
-        res.render('nuestrasRecetas', {recetas:recetas});
-    },
-    
-    
+    }
 };
 
 module.exports = controllerMain;
