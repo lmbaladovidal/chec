628c4535492a36b3c87545c5268c7400a34bb4ea
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-06-13 20:43:37 -0300

    inicio modelo recetas

---
+++ DataBase/models/Recipe.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Recipes';
+    let cols = {
+
+    };
+    let config = {
+        timestamps: false
+    }
+
+}
+
+const Recicpe = sequelize.define(alias, cols, config)
