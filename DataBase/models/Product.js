74f22334392ed3d3cd8861178dac01f6cbf5ab09
David Juncos <davidjuncos@hotmail.com>
2022-06-24 10:18:48 -0300

    sequalize de sales y shoppingcart y sus dependencias

--- DataBase/models/Product.js
+++
@@ -1,8 +0,0 @@
-module.exports = 
-    (sequelize,DataTypes)=>{
-        let alias = "Products"
-        let columns = {}
-        let config ={}
-        const Products = sequelize.define(alias,columns,config);
-        //asociaciones
-    }
