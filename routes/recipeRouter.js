b8a068e2d51736ea2812627eb70f95586c6cab2a
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-06-26 21:02:17 -0300

    avances controlador Recetas pero no funciona create, delete ni update

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -6,11 +6,10 @@ const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/nuestrasRecetas', recipeController.recetasDetail);
-
-// router.get('/nuestrasRecetasAdmin/',authMiddleware,recipeController.recetaCreate);
-// router.post('/nuestrasRecetasAdmin/',authMiddleware, recipeController.recetaCreate);
-// router.get('/nuestrasRecetasAdmin/:id',authMiddleware,recipeController.recetaAdmin);
-// router.put('/nuestrasRecetasAdmin/:id', authMiddleware,  recipeController.recetaUpdate);
+router.get('/nuestrasRecetasAdmin/',authMiddleware,recipeController.recetaCreate);
+router.post('/nuestrasRecetasAdmin/',authMiddleware, recipeController.recetaCreate);
+router.get('/nuestrasRecetasAdmin/:id',authMiddleware,recipeController.recetaAdmin);
+router.put('/nuestrasRecetasAdmin/:id', authMiddleware,  recipeController.recetaUpdate);
 // router.delete('/nuestrasRecetasAdmin/:id', authMiddleware,recipeController.recetaDelete);
 
 module.exports = router;
