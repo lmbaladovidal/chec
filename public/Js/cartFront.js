881f1a390e068228a68e7da66054dea047e9c58b
David Juncos <davidjuncos@hotmail.com>
2022-07-16 23:20:04 -0300

    mergin para ara

--- public/Js/cartFront.js
+++ public/Js/cartFront.js
@@ -1,23 +1,64 @@
 window.onload = () => {
-        const a = []
+
+    const formCart = document.getElementById("formProductCart");
+    const inputs = document.querySelectorAll(".spinButton");
+    const buttons = document.querySelectorAll('#formProductCart button')
+    const add = document.getElementById('add')
+
+    const price5 = parseInt(document.getElementById('price5'))
+    const quantity5 = parseInt(document.getElementById('quantity5'))
+    
+
+    const quantity = quantity5.value
+    const price = price5.value 
+        console.log(price5);
+        console.log(quantity);
         
-        //const inputs = document.querySelectorAll("#formRegister input");
-        //const inputs = document.querySelectorAll("#formProductCart input");
+        add.addEventListener('click', function calcular(){
+      
+
+
+            let quantitySub= quantity + 1
+            let priceSub = quantitySub * price
+             return priceSub
+            })
+             
+        //console.log(price);
+
+        // const input = event.target;
+        // input.value <= 0 ? (input.value = 1) : null;
         
-        //inputs.forEach((input) => {
-       
-        //input.addEventListener("keyup", validarFormulario);
-        //input.addEventListener("blur", validarFormulario);
-        //input.addEventListener("change", valExtFile);
+    }
+   
+      
+    // const a = []
+        
+    // console.log(inputs);
+    // inputs.forEach((input) => {
+    //     input.addEventListener("click", operaciones);
+    // });
+   
+    //     function calculate(){
+    //         formProductCart.addEventListener("submit", (e) => {
+    //             e.preventDefault(); 
+    //             console.log(e) 
+    //             Object.values(campos).filter(value=>{return !value}).length == 0?formProductCart.submit():null;
+    //         });
+    //     }
+        
 
-  //});
+    //     function calculate(e){  
 
-        function calculate(id){  
+            
+    // //     elemento = document.getElementById("price" + id);
+    // //     elemento = document.getElementById(e.target.id)
+    // //     console.log("ENTRO PRECIO $ " + elemento.value );
+    // //     counter = document.getElementById("spinButton" + id).value;
+    // //     console.log(counter)
+    // //     console.log (counter * price )
+    // //     elemento.textContent = "$ " + price.slice(2,price.length)*counter
+      
 
-            elemento = document.getElementById("price" + id);
-            counter = document.getElementById("spinButton" + id).value;
-            console.log(counter)
-            const price = document.getElementById("price" + id).textContent
+      
 
-            elemento.textContent = "$ " + price.slice(2,price.length)*counter}
-}
+    //     }
