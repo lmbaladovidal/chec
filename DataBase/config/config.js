74d205e5c06ec3a3da26730ceae7eea1137686d8
AlbaMoran <alba.morann@gmail.com>
2022-08-03 16:12:01 -0300

    para pull Leandro

--- DataBase/config/config.js
+++ DataBase/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "Dworkin1946", 
+    "password": "camba", 
     "database": "chec_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
